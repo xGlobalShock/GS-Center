25b4fd71f6bb03c595b883082850d4e13ca8b7ee
xGlobalShock <xGlobalShock@gmail.com>
2026-04-16 15:40:19 +0300

    Updated

--- main-process/windowManager.js
+++ main-process/windowManager.js
@@ -153,7 +153,7 @@ function createWindow() {
         contextIsolation: true,
         enableRemoteModule: false,
         webviewTag: true,
-        devTools: true, // DISABLED IN PRODUCTION
+        devTools: false, // DISABLED IN PRODUCTION
         backgroundThrottling: false,
       },
     });
@@ -165,18 +165,18 @@ function createWindow() {
     mainWindow.on('unmaximize', () => mainWindow?.webContents.send('window-maximized-changed', false));
 
     // DISABLED/ENABLED DEVTOOLS IN PRODUCTION 
-    // mainWindow.webContents.on('before-input-event', (event, input) => {
-    //   if (
-    //     input.control &&
-    //     input.shift &&
-    //     (input.key.toLowerCase() === 'i' || input.key.toLowerCase() === 'c' || input.key.toLowerCase() === 'j')
-    //   ) {
-    //     event.preventDefault();
-    //   }
-    //   if (input.key === 'F12') {
-    //     event.preventDefault();
-    //   }
-    // });
+    mainWindow.webContents.on('before-input-event', (event, input) => {
+      if (
+        input.control &&
+        input.shift &&
+        (input.key.toLowerCase() === 'i' || input.key.toLowerCase() === 'c' || input.key.toLowerCase() === 'j')
+      ) {
+        event.preventDefault();
+      }
+      if (input.key === 'F12') {
+        event.preventDefault();
+      }
+    });
 
 
     // Track page-load completion BEFORE loadURL — avoids race where
